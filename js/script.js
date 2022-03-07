7ab0f489be3fc9aa6a4a4623bccd6d065ebf2f67
briankc804 <briankc804@gmail.com>
2022-03-07 04:07:29 +0300

    made changes on script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 // Business (or back-end) logic:
-var akan = function(y, m, d, g) {
+var akan = function(year, month, day, gender) {
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var d = new Date(y, --m, d);
+    var date= new Date(year, --month, date);
     if (g === "Female") {
-        return d && femaleNames[d.getDay()];
+        return date && femaleNames[date.getDay()];
     } else {
         return d && maleNames[d.getDay()];
     }
 }
 // User interface (or front-end) logic:
- $(document).ready(function() {
-     $("form#form").submit(function(event) {
+ (document).ready(function() {
+     ("form#form").submit(function(event) {
          event.preventDefault();
          var y = parseInt($("#year").val());
          var m = parseInt($("#month").val());
          var d = parseInt($("#date").val());
-         var g = $("input:radio[name=gender]:checked").val();
+         var g = ("input:radio[name=gender]:checked").val();
          var result = akan(y, m, d, g);
          alert("Your akan name is: " + result);
          //refresh page
